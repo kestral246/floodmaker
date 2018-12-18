e849ffe189f6b05771712c1d02fd760e5f9a3b58
David G <kestral246@gmail.com>
2018-12-17 17:45:36 -0700

    Sneak right click to flood.

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- Floodmaker - A mod to flood undersea caverns.
--- 2018-12-15
+-- 2018-12-17
 
 -- Copyright (C) 2018 David G (kestral246@gmail.com)
 
@@ -14,7 +14,10 @@
 -- To use:
 -- Only works in creative mode.  No crafting recipe.
 -- Max_water_level should be set to sea level or lower, by default y=1, to avoid catastrophic flooding.
--- Point at node and right click.
+-- Point at node and right click to get summary of what would be flooded.
+--     A large number of existing water nodes suggests possible connection to the sea.
+-- To actually flood, hold sneak key (default shift) while left clicking.
+--     Not guaranteed to be the same as summary, since map blocks could load or unload, or other players could have changed the terrain.
 -- The node clicked will turn to water, and then the flood will propagate through all
 -- adjacent air and water nodes, but won't go any higher than the initial node.
 
@@ -22,6 +25,7 @@
 local scanned = {}			-- Set containing scanned nodes, so they don't get scanned multiple times.
 local tocheck = {}			-- Table of nodes to check.
 local toflood = {}			-- Table of nodes that need to be flooded.
+local watercount = {}
 local max_water_level = 1	-- Define as sea level or lower, for safety.
 local maxcount = 250000		-- Maximum number of nodes to check.
 
@@ -30,6 +34,7 @@ minetest.register_on_joinplayer(function(player)
 	scanned[pname] = {}
 	tocheck[pname] = {}
 	toflood[pname] = {}
+	watercount[pname] = 0
 end)
 
 minetest.register_on_leaveplayer(function(player)
@@ -37,6 +42,7 @@ minetest.register_on_leaveplayer(function(player)
 	scanned[pname] = nil
 	tocheck[pname] = nil
 	toflood[pname] = nil
+	watercount[pname] = nil
 end)
 
 -- Encode/decode xyz location into a single number to minimize memory usage of above tables.
@@ -103,6 +109,7 @@ local check_node = function(pname, pos, ymax)
 	if (cover or pos.y == ymax) and not string.match(name, "water_source") then
 		table.insert(toflood[pname], enc_pos)
 	end
+	if string.match(name, "water") then watercount[pname] = watercount[pname] + 1 end -- for statistics
 end
 
 minetest.register_tool("floodmaker:floodmaker", {
@@ -113,33 +120,41 @@ minetest.register_tool("floodmaker:floodmaker", {
 		local pname = player:get_player_name()
 		-- Only works in creative mode.
 		if creative and creative.is_enabled_for and creative.is_enabled_for(pname) then
+			local do_it = false
+			if player:get_player_control().sneak then
+				do_it = true
+			end
 			-- Initialize temporary tables for safety.
 			scanned[pname] = {}
 			tocheck[pname] = {}
 			toflood[pname] = {}
+			watercount[pname] = 0
 			local pos = pointed_thing.under
 			-- Only works if pointing to node at or below water level.
 			if pos.y <= max_water_level then
-				-- Pointed to node is changed to first water source.
-				minetest.set_node(pos, {name="default:water_source"})
+				-- Pointed to node will be changed to water.
 				table.insert(tocheck[pname], encode(pos))
-				check_node(pname, pos, pos.y)
-				-- Check rest of nodes added during scan process.
-				local count = 2
+				local count = 1
 				while count <= table.getn(tocheck[pname]) and count <= maxcount do
 					check_node(pname, decode(tocheck[pname][count]), pos.y)  -- fifo
 					count = count + 1
 				end
-				-- Print statistics.
-				if count == 2 then count = 1 else count = count - 2 end
-				minetest.debug("floodmaker: y = "..tostring(pos.y)..", scan = "..tostring(tlength(scanned[pname]))..", check = "..tostring(count)..", flood = "..tostring(tlength(toflood[pname])+1))
-				minetest.chat_send_player(pname, "floodmaker: height = "..tostring(pos.y)..", flood = "..tostring(tlength(toflood[pname])+1)..", total = "..tostring(count))
-				-- Add water sources to all nodes flagged for flooding.
-				for _,v in ipairs(toflood[pname]) do
-					local fpos = decode(v)
-					minetest.set_node(fpos, {name="default:water_source"})
+				count = count - 1
+				if do_it then
+					-- Print statistics.
+					minetest.chat_send_player(pname, "floodmaker: flooded "..tostring(count).." nodes, of which "..tostring(watercount[pname]).." were already water.")
+					minetest.debug("floodmaker: y = "..tostring(pos.y)..", scan = "..tostring(tlength(scanned[pname]))..", check = "..tostring(count)..", flood = "..tostring(tlength(toflood[pname]))..", already H20 = "..tostring(watercount[pname]))
+					-- Add water sources to all nodes flagged for flooding.
+					for _,v in ipairs(toflood[pname]) do
+						local fpos = decode(v)
+						minetest.set_node(fpos, {name="default:water_source"})
+					end
+				else
+					-- Print statistics.
+						minetest.chat_send_player(pname, "floodmaker: would flood "..tostring(count).." nodes, of which "..tostring(watercount[pname]).." are already water. (height = "..tostring(pos.y)..")")
+						minetest.chat_send_player(pname, "Press sneak (shift) while right-clicking to flood.")
 				end
-			else
+			else  -- too high
 				minetest.chat_send_player(pname, "floodmaker: height = "..tostring(pos.y)..", needs to be less than or equal to "..tostring(max_water_level))
 			end
 			scanned[pname] = {}  -- Clear temporary tables, which could be large.
